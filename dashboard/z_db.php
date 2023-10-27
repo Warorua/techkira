b6410fcfd81203310a07a773d9d59008b1de7f22
Warorua <waroruaalex640@gmail.com>
2023-10-27 23:50:49 +0300

    tyjrtujk

--- dashboard/z_db.php
+++ dashboard/z_db.php
@@ -1,11 +1,11 @@
 <?php
-$con = new mysqli("localhost", "root", "", "vogue");
+$con = new mysqli("45.84.206.68", "tsavosit_techkira", "wKAogjb?U4*M", "tsavosit_techkira");
 if ($con->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
 //Your Website URL Goes Here
-$url="http://localhost/vogue";
+$url="http://localhost/techkira";
 
 
 //Set Blog Activation Bonus Here (It must be only Number)
